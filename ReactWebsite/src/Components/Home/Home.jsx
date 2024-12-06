ddd1928af8e431e3f9c67480db3371b2aaa1c789
MartinNackademin <mamoh55@gmail.com>
2024-12-06 18:04:38 +0100

    Added section rules to index, added home styling and content

--- ReactWebsite/src/Components/Home/Home.jsx
+++ ReactWebsite/src/Components/Home/Home.jsx
@@ -5,13 +5,29 @@ import './Home.css';
     export const Home = () => {
         return (
             <main>
-                
+               
                 <img id ="profilepicture" src="https://imgcdn.stablediffusionweb.com/2024/9/17/76ce3fbb-5c8e-48d7-8959-4d74ca5106c0.jpg" alt="placeholder" 
                 />
+                
                 <section>
                 
-                <div><h1>Hello, I'm </h1><h1>Martin</h1></div>
-                <div> <p>This is the Home Page</p> </div> 
+                <h1 id = "hometitle">
+         <span>Hello, I'm</span>{' '}<span>Martin</span> <span>{'/>'}</span>
+         
+              </h1>
+   
+                </section>
+
+                <section>
+                </section>
+                
+                <section id="subheader">
+                  
+                <p>
+              Here, you can check out some of my projects,
+              learn more about me, or get in touch.
+             </p>   
+
 
                 </section>
 
