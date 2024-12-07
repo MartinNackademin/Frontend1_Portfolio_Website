9235707b81f6a05f5a1f15ff411b7f8d854fcaf7
MartinNackademin <mamoh55@gmail.com>
2024-12-07 22:59:45 +0100

    added footer content and stlye

--- ReactWebsite/src/Components/Footer/Footer.jsx
+++ ReactWebsite/src/Components/Footer/Footer.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
 function Footer() {
   const currentYear = new Date().getFullYear();
     return (
     <>
       <footer>
-        (C) Martin Mohns {currentYear}
+        (C) {currentYear}  Martin Mohns. All Cats Reserved.
       </footer>    
     </>
     )
